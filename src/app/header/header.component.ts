a615a12da979e331e655fc721543ba89b4fa9496
Himanshu - (Digital) <HI953776@wipro.com>
2019-02-20 16:51:06 +0530

    office

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector:'app-header',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
 
 })
 export class HeaderComponent{
+  featureSelected = new EventEmitter
+onSelect(feature:String){
 
+}
 }
